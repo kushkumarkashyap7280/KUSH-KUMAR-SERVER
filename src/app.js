dec8559d4577c7435cc2049bf96db076bc496641
kush <kushkumarking098765@gmail.com>
2025-08-17 12:30:09 +0530

    same prblem again and  again

--- src/app.js
+++ src/app.js
@@ -41,8 +41,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// Express 5 path-to-regexp does not accept '*' here; register without a path to catch all
-app.options(cors(corsOptions));
 
 app.use(express.json({limit : "16mb"}));
 app.use(express.urlencoded({extended: true, limit : "16mb"}));
