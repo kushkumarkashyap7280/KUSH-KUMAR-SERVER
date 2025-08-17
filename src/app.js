c85daddab60fdf851daa5cab361c269196e8fe10
kush <kushkumarking098765@gmail.com>
2025-08-17 12:24:14 +0530

    again cookie issue day 17 aug

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// Express 5 path-to-regexp does not accept '*' here; register without a path to catch all
+app.options(cors(corsOptions));
 
 app.use(express.json({limit : "16mb"}));
 app.use(express.urlencoded({extended: true, limit : "16mb"}));
